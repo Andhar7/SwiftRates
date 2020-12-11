46e5275159e2abbcce2bda74526b05e1484c1f6d
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-11 18:55:32 +0100

    Initial Commit

---
+++ SwiftRates/SwiftRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRatesApp.swift
+//  SwiftRates
+//
+//  Created by Andrej Kling on 11.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
